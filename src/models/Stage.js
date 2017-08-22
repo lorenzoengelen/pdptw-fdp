ce3eaf2faba2cd3f52c4542a04e180735cf97ccb
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-22 17:58:40 +0200

    further dev for BitSet

--- src/models/Stage.js
+++ src/models/Stage.js
@@ -1,4 +1,25 @@
-function Stage() {
+const State = require('./State.js');
+const HashMap = require('../structs/HashMap.js');
+
+const Stage = function(problem) {
+  this.problem = problem;
+  this.numOrders = problem.getNumberOfOrders();
+  
+  // called Sets in paper
+  this.states = new HashMap();
+};
+
+Stage.prototype.createInitialStage = function() {
+  let state;
+  for (let i = 1; i <= this.numOrders; i++) {
+    state = new State(this.problem, i);
+  }
+};
+
+Stage.prototype.addNewState = function(oldState, newTermNode) {
+};
+
+Stage.prototype.criteria4 = function(state, loc, k) {
 
 };
 
--- src/models/State.js
+++ src/models/State.js
@@ -1,7 +1,27 @@
 const BitSet = require('../structs/BitSet.js');
+const HashMap = require('../structs/HashMap.js');
 
-const State = function() {
+const State = function(problem, jPickup, visited, canVisit, load) {
+  this.problem = problem;
+  this.numOrders = problem.getNumberOfOrders();
 
+  this.visited = null;
+  this.canVisit = null;
+  this.load = 0;
+  this.terminalNodes = new HashMap();
+
+  if (arguments.length === 2) {
+    // for initial stage (Pickup Nodes)
+    this.visited = new BitSet(2 * this.numOrders);
+    this.canVisit = new BitSet(2 * this.numOrders);
+
+    console.log(this.visited.get(10));
+    this.visited.toggle(10);
+    console.log(this.visited.get(10));
+
+  } else if (arguments.length === 4) {
+
+  }
 };
 
 module.exports = State;
