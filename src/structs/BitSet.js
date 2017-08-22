d5e8bf562452203837d9a4c32e357df4bb044b27
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-22 15:02:48 +0200

    require a new data structure; BitSet

---
+++ src/structs/BitSet.js
@@ -0,0 +1,32 @@
+const BitSet = function() {
+
+};
+
+BitSet.prototype.and = function(set) {
+};
+
+BitSet.prototype.andNot = function(set) {
+};
+
+BitSet.prototype.or = function(set) {
+};
+
+BitSet.prototype.xor = function(set) {
+};
+
+BitSet.prototype.set = function(pos) {
+};
+
+BitSet.prototype.get = function(pos) {
+};
+
+BitSet.prototype.clear = function(pos) {
+};
+
+BitSet.prototype.flip = function(from, to) {
+
+};
+
+BitSet.prototype.clone = function() {
+
+};
