04f54c1c0411356956a0824aeb9f590058fa4671
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-22 17:58:15 +0200

    stuck at BitSet

--- src/structs/BitSet.js
+++ src/structs/BitSet.js
@@ -1,7 +1,21 @@
-const BitSet = function() {
+const dataSymbol = Symbol('data');
+const BITS = 32;
 
+const BitSet = function(size, value) {
+  this.size = size;
+  let ints = Math.ceil(this.size / BITS);
+  this[dataSymbol] = Array.apply(null, Array(ints)).map(x => !value ? 0 : -1);
 };
 
+// private methods
+BitSet.prototype._convert = function(n) {
+  if (n > this.size) return null;
+  let byte = n / BITS | 0;
+  let bit = n % BITS;
+  return {byte, bit};
+};
+
+// public mehthods
 BitSet.prototype.and = function(set) {
 };
 
@@ -17,16 +31,26 @@ BitSet.prototype.xor = function(set) {
 BitSet.prototype.set = function(pos) {
 };
 
-BitSet.prototype.get = function(pos) {
+BitSet.prototype.get = function(n) {
+  let pos = this._convert.call(this, n);
+  return pos ? (this[dataSymbol][pos.byte] & 1 << pos.bit) > 0 : null;
 };
+
+BitSet.prototype.toggle = function(n) {
+  let pos = this._convert.call(this, n);
+  if (pos) this[dataSymbol][pos.byte] ^= 1 << pos.bit;
+  return pos != null;
+}
 
 BitSet.prototype.clear = function(pos) {
 };
 
+// Reverses the bits from startIndex to endIndex
 BitSet.prototype.flip = function(from, to) {
-
 };
 
+// Duplicates the invoking BitSet object
 BitSet.prototype.clone = function() {
-
 };
+
+module.exports = BitSet;
