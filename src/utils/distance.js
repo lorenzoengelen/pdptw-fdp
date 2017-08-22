634461f8a50a90f761de420e33f161f56761237a
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-22 10:23:00 +0200

    updated distance computation methods default is Euclidean distance

--- src/utils/distance.js
+++ src/utils/distance.js
@@ -1,4 +1,10 @@
-exports.euclideanDistance = function(x1, y1, x2, y2) {
+const EUCLIDEAN = 'EUCLIDEAN_DISTANCE';
+const HAVERSINE = 'HAVERSINE_DISTANCE';
+const GOOGLE = 'GOOGLE_DISTANCE';
+
+const DISTANCE_COMPUTATION_METHOD = EUCLIDEAN;
+
+const euclideanDistance = function(x1, y1, x2, y2) {
   let dX = Math.abs(x1 - x2);
   let dY = Math.abs(y1 - y2);
   dX *= dX;
@@ -6,10 +12,29 @@ exports.euclideanDistance = function(x1, y1, x2, y2) {
   return Math.sqrt(dX + dY);
 };
 
-exports.haversineDistance = function(x1, y1, x2, y2) {
+const haversineDistance = function(lon1, lat1, lon2, lat2) {
+  // TODO
 };
 
-exports.googleDistance = function(x1, y1, x2, y2) {
-  // make call to Google Maps api
+exports.googleDistance = function(lon1, lat1, lon2, lat2) {
+  // TODO
 };
 
+const distance = function(x1, y1, x2, y2) {
+  switch (DISTANCE_COMPUTATION_METHOD) {
+    case EUCLIDEAN:
+      return euclideanDistance(x1, y1, x2, y2);
+      break;
+    case HAVERSINE:
+      return haversineDistance(x1, y1, x2, y2);
+      break;
+    case GOOGLE:
+      return googleDistance(x1, y1, x2, y2);
+      break;
+    default:
+      return euclideanDistance(x1, y1, x2, y2);
+      break;
+  }
+};
+
+module.exports = distance;
