c40ab2ad84e9523b7a827b8e96973fed0e384d6f
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-22 11:36:44 +0200

    make reference to locations by unique id

--- src/models/Problem.js
+++ src/models/Problem.js
@@ -60,15 +60,18 @@ Problem.prototype.createProblem = function(instance) {
   let id = 0;
 
   const {vehicleId, depotLocationLon, depotLocationLat, earliestStartTime, latestEndTime} = vehicle;
-  const depot = new Location(id++, `vehicle-${vehicleId}`, depotLocationLon, depotLocationLat, 0, earliestStartTime, latestEndTime, 0, 0, 0);
+  const depot = new Location(id++, `depot-${vehicleId}`, depotLocationLon, depotLocationLat, 0, earliestStartTime, latestEndTime, 0, 0, 0);
   this.locations.push(depot);
+  this.refLocations.put(depot.getReference(), depot.getId());
 
   orders.forEach((order, i) => {
     const {orderId, pickupLocationLon, pickupLocationLat, deliveryLocationLon, deliveryLocationLat, orderLoad, earliestPickupTime, latestPickupTime, earliestDeliveryTime, latestDeliveryTime, pickupServiceTime, deliveryServiceTime} = order;
-    const pickup = new Location(id++, `order-${orderId}`, pickupLocationLon, pickupLocationLat, orderLoad, earliestPickupTime, latestPickupTime, pickupServiceTime, 0, id);
-    const delivery = new Location(id++, `order-${orderId}`, deliveryLocationLon, deliveryLocationLat, orderLoad * -1, earliestDeliveryTime, latestDeliveryTime, deliveryServiceTime, id - 2, 0);
+    const pickup = new Location(id++, `pickup-${orderId}`, pickupLocationLon, pickupLocationLat, orderLoad, earliestPickupTime, latestPickupTime, pickupServiceTime, 0, id);
+    const delivery = new Location(id++, `delivery-${orderId}`, deliveryLocationLon, deliveryLocationLat, orderLoad * -1, earliestDeliveryTime, latestDeliveryTime, deliveryServiceTime, id - 2, 0);
     this.locations.push(pickup);
     this.locations.push(delivery);
+    this.refLocations.put(pickup.getReference(), pickup.getId());
+    this.refLocations.put(delivery.getReference(), delivery.getId());
   });
 
   this._calcDistTime();
