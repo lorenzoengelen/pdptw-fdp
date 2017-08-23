5f98a329afd620bfbb8ce38d30923579a123d035
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-23 15:16:13 +0200

    finished Label class

--- src/models/Label.js
+++ src/models/Label.js
@@ -1,5 +1,9 @@
-const Label = function() {
-
+const Label = function(termNode, time, distance, previousLabel) {
+  this.time = time;
+  this.distance = distance;
+  this.eliminated = false;
+  this.termNode = termNode;
+  this.previousLabel = previousLabel;
 };
 
 module.exports = Label;
