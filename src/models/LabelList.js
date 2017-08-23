504e27a7e37dcf8878f49bb829c0530caf64bc16
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-23 15:25:10 +0200

    enable LabelList to add Labels

--- src/models/LabelList.js
+++ src/models/LabelList.js
@@ -1,5 +1,15 @@
-const LabelList = function() {
+const ArrayList = require('../structs/ArrayList.js');
 
+const LabelList = function() {
+  this.labelList = new ArrayList();
+};
+
+LabelList.prototype.addLabel = function(label) {
+  this.labelList.add(label);
+};
+
+LabelList.prototype.getLabelList = function() {
+  return this.labelList;
 };
 
 module.exports = LabelList;
