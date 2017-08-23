36ab32673b1915c7852a0ce46ced8c9e6c038336
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-23 15:35:11 +0200

    updated TerminalNode enable to add label to LabelList

--- src/models/TerminalNode.js
+++ src/models/TerminalNode.js
@@ -1,5 +1,12 @@
-const TerminalNode = function() {
+const LabelList = require('./LabelList.js');
 
+const TerminalNode = function(termNode) {
+  this.terminalNode = termNode;
+  this.labelList = new LabelList();
+};
+
+TerminalNode.prototype.addLabel = function(label) {
+  this.labelList.addLabel(label);
 };
 
 module.exports = TerminalNode;
