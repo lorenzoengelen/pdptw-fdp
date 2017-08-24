3b6b4bf3ed7e1c2d20db9ff4c89f4f5e1f51c91c
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-24 15:51:37 +0200

    testing criteria 4 for determining terminal node

--- src/models/Solver.js
+++ src/models/Solver.js
@@ -20,13 +20,13 @@ Solver.prototype.solve = function(instance, timeout) {
   stage.createInitialStage();
   this.stages.add(stage);
 
-  // final stage
-  const finalStage = new Stage(this.problem);
-
   // stages
   for (let i = 2, len = this.problem.getNumberOfLocations(); i <= len; i++) {
+    
     const prevStage = this.stages.get(i - 2);
     const newStage = new Stage(this.problem);
+    const finalStage = new Stage(this.problem);
+
     if (i === len) {
       // final stage
 
@@ -37,17 +37,25 @@ Solver.prototype.solve = function(instance, timeout) {
         if (Date.now() > end) {
           return 'exceeded time limit';
         }
-        // console.log(state.getCanVisit().toString());
-        // console.log(state.getCanVisit().nextSetBit());
+        // ===> HERE <=== 
+        state.getCanVisit().print(); // console log
+        // LOOP over every location that can be visited from a specific state
+        for (let loc = state.getCanVisit().nextSetBit(0); loc >= 0; loc = state.getCanVisit().nextSetBit(loc + 1)) {
+          if (Date.now() > end) {
+            return 'exceeded time limit';
+          }
+          // CRITERIA #4 - POSSIBLE TO VISIT UNVISITED NODES WHILE RESPECTING TIME CONSTRAINT
+          if (newStage.criteria4(state, loc, i)) {
+            // POSSIBLE to visit specific location; k-2, to k+4 nodes sorted on earliest due date can be visited without time window violation
+            // ===> HERE <===
+            // newStage.addNewState(state, loc);
+          }
+
+        }
       });
     }
     this.stages.add(newStage);
   }
-
-  var state = new State(this.problem, 0);
-  console.log('==============')
-  console.log(state.getCanVisit().nextSetBit());
-
 
   return '';
 };
