5ed3081fd9b5e98d0de9c2accdb60886c447bee6
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-24 15:37:53 +0200

    implemented constraint #4

--- src/models/Stage.js
+++ src/models/Stage.js
@@ -17,10 +17,27 @@ Stage.prototype.createInitialStage = function() {
   }
 };
 
+// CRITERIA #3 - CAPACITY CONSTRAINT
 Stage.prototype.addNewState = function(oldState, newTermNode) {
 };
 
+// CRITERIA #4 - POSSIBLE TO VISIT UNVISITED NODES WHILE RESPECTING TIME CONSTRAINT
 Stage.prototype.criteria4 = function(state, loc, k) {
+  const earliestTime = this.problem.getLocation(loc).getEarliestServiceTime();
+  // the test is carried out for the most probable nodes visited at iteration k+1;
+  // i.e. unvisited nodes from k-2, to k+2
+  const minIter = Math.max(k - 2, 0);
+  const maxIter = Math.min(k + 4, this.problem.getNumberOfLocations() - 1);
+
+  for (let iter = minIter; iter <= maxIter; iter++) {
+    const newLoc = this.problem.getSortedLocation(iter).getId();
+    if (!state.isVisited(newLoc) && loc !== newLoc) {
+      if (this.problem.getLocation(newLoc).getLatestServiceTime() < earliestTime + this.problem.getServiceTimeDuration(loc) + this.problem.getTime(loc, newLoc)) {
+        return false;
+      }
+    }
+  }
+  return true;
 };
 
 Stage.prototype.getStates = function() {
