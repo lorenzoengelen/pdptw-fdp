3d75a6ee2b4122b071bbf623815bc32a7e030141
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-25 11:51:50 +0200

    allow terminal node to add new labels

--- src/models/TerminalNode.js
+++ src/models/TerminalNode.js
@@ -9,6 +9,10 @@ TerminalNode.prototype.addLabel = function(label) {
   this.labelList.addLabel(label);
 };
 
+TerminalNode.prototype.addNewLabels = function(oldLabels) {
+  this.labelList.addNewLabels(oldLabels, this.terminalNode);
+};
+
 TerminalNode.prototype.getLabelList = function() {
   return this.labelList.getLabelList();
 };
