d41d6a502bdcaa888ef2719aa21f008d80b3a2be
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-31 13:17:05 +0200

    wip crit 8

--- src/models/TerminalNode.js
+++ src/models/TerminalNode.js
@@ -22,10 +22,10 @@ TerminalNode.prototype.checkPostFeasibility = function(state, iter) {
   for (let i = 0; i < this.labelList.getLabelList().size(); i++) {
     const label = this.labelList.getLabelList().get(i);
     if (!this.criteria6(state, iter, label)) {
+      this.labelList.getLabelList().remove(i);
+    } else if (!this.criteria8(state, label)) {
       // TODO
     } else if (!this.criteria7(state, label)) {
-      // TODO
-    } else if (!this.criteria8(state, label)) {
       // TODO
     }
   }
@@ -38,8 +38,8 @@ TerminalNode.prototype.criteria6 = function(state, k, label) {
   
   for (let iter = minIter; iter <= maxIter; iter++) {
     const newLoc = this.problem.getSortedLocation(iter).getId();
-    if (!state.isVisited(newLoc) && label.termNode !== newLoc) {
-      if (label.time + this.problem.getServiceTimeDuration(label.termNode) + this.problem.getTime(label.termNode, newLoc) > this.problem.getLocation(newLoc).getLatestServiceTime()) {
+    if (!state.isVisited(newLoc) && label.getTermNode() !== newLoc) {
+      if (label.getTime() + this.problem.getServiceTimeDuration(label.getTermNode()) + this.problem.getTime(label.getTermNode(), newLoc) > this.problem.getLocation(newLoc).getLatestServiceTime()) {
         return false;
       }
     }
@@ -51,9 +51,44 @@ TerminalNode.prototype.criteria6 = function(state, k, label) {
 //                AND N+L2 WHOSE ORIGINS L1, AND L2 HAVE BEEN PREVIOUSLY VISITED 
 //                WHILE RESPECTING THE TIME CONSTRIANTS
 TerminalNode.prototype.criteria7 = function(state, label) {
+
 };
 
+// CRITERIA #8 -  IF NODE J IS VISITED AT TIME Tj, IT MUST BE POSSIBLE TO VISIT
+//                ALL PAIRS OF UNIVISTED ORIGINS L1, AND L2 WHILE RESPECTING THE
+//                TIME CONSTRAINTS
 TerminalNode.prototype.criteria8 = function(state, label) {
+  let pick1 = -1, pick2 = -1, pick;
+  let found1 = false, found2 = false;
+  let index = 0;
+  
+  while ((!found1 && !found2) && index < this.problem.getNumberOfOrders()) {
+    if (!found1) {
+      pick = this.problem.getSortedPickupLocation(index).getId();
+      if (!state.isVisited(pick)) {
+        pick1 = pick;
+        found1 = true;
+        index++;
+      }
+    }
+    if (found1 && index < this.problem.getNumberOfOrders()) {
+      pick = this.problem.getSortedPickupLocation(index).getId();
+      if (!state.isVisited(pick)) {
+        pick2 = pick;
+        found2 = true;
+      }
+    }
+    index++;
+  }
+
+  if (found1 && found2) {
+    let time1 = Math.max(this.problem.getLocation(pick1).getEarliestServiceTime(), label.getTime() + this.problem.getServiceTimeDuration(label.getTermNode()) + this.problem.getTime(label.getTermNode(), pick1));
+    console.log(this.problem.getLocation(pick1).getEarliestServiceTime());
+    console.log(label.getTime() + this.problem.getServiceTimeDuration(label.getTermNode()) + this.problem.getTime(label.getTermNode(), pick1))
+    let time2 = Math.max();
+  } else {
+    return true;
+  }
 };
 
 TerminalNode.prototype.getLabelList = function() {
