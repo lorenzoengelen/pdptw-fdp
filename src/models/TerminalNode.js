74048db2f59459e223877380c79d018349b14488
Lorenzo Engelen <lorenzoengelen@gmail.com>
2017-08-31 08:17:52 +0200

    finished crit 6

--- src/models/TerminalNode.js
+++ src/models/TerminalNode.js
@@ -18,17 +18,42 @@ TerminalNode.prototype.eliminateLabels = function() {
   this.labelList.eliminateLabels();
 };
 
-TerminalNode.prototype.checkPostFeasibility = function() {
-  console.log(this.labelList.getLabelList().size())
+TerminalNode.prototype.checkPostFeasibility = function(state, iter) {
+  for (let i = 0; i < this.labelList.getLabelList().size(); i++) {
+    const label = this.labelList.getLabelList().get(i);
+    if (!this.criteria6(state, iter, label)) {
+      // TODO
+    } else if (!this.criteria7(state, label)) {
+      // TODO
+    } else if (!this.criteria8(state, label)) {
+      // TODO
+    }
+  }
 };
 
-TerminalNode.prototype.criteria6 = function() {
+// CRITERIA #6 - IT MUST BE POSSIBLE TO VISIT EACH UNVISITED NODE
+TerminalNode.prototype.criteria6 = function(state, k, label) {
+  const minIter = Math.max(k - 2, 0);
+  const maxIter = Math.min(k + 4, this.problem.getNumberOfLocations());
+  
+  for (let iter = minIter; iter <= maxIter; iter++) {
+    const newLoc = this.problem.getSortedLocation(iter).getId();
+    if (!state.isVisited(newLoc) && label.termNode !== newLoc) {
+      if (label.time + this.problem.getServiceTimeDuration(label.termNode) + this.problem.getTime(label.termNode, newLoc) > this.problem.getLocation(newLoc).getLatestServiceTime()) {
+        return false;
+      }
+    }
+  }
+  return true;
 };
 
-TerminalNode.prototype.criteria7 = function() {
+// CRITERIA #7 -  IT MUST BE POSSIBLE TO VISIT ALL PAIRS OF DESTINATIONS N+L1,
+//                AND N+L2 WHOSE ORIGINS L1, AND L2 HAVE BEEN PREVIOUSLY VISITED 
+//                WHILE RESPECTING THE TIME CONSTRIANTS
+TerminalNode.prototype.criteria7 = function(state, label) {
 };
 
-TerminalNode.prototype.criteria8 = function() {
+TerminalNode.prototype.criteria8 = function(state, label) {
 };
 
 TerminalNode.prototype.getLabelList = function() {
